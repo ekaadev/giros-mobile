46101acfb54165a00c22886e4cd89f86224973bc
Yohanes Eka Putra <ekaadev9@gmail.com>
2024-10-27 15:41:17 +0700

    Feat: add layout and widget github roast

--- app/src/main/java/eka/giros/MainActivity.kt
+++ app/src/main/java/eka/giros/MainActivity.kt
@@ -1,22 +1,40 @@
 package eka.giros
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import eka.giros.ui.theme.GirosTheme
 
 class MainActivity : ComponentActivity() {
+    private lateinit var usernameButton: Button
+    private lateinit var usernameEditText: EditText
+
+
+    private fun initComponent() {
+        usernameButton = findViewById(R.id.usernameButton)
+        usernameEditText = findViewById(R.id.usernameEditText)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.github)
+
+        initComponent()
+
+        usernameButton.setOnClickListener {
+            usernameButton.isEnabled = false
+            usernameButton.text = resources.getString(R.string.loadingText)
+
+
+            Handler(Looper.getMainLooper()).postDelayed({
+                usernameButton.text = resources.getString(R.string.usernameButton)
+                usernameButton.isEnabled = true
+            }, 3000)
+        }
     }
 }
