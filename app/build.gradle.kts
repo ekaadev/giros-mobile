ed275314ced16ad8a07d2c02a70c217ed7e48c8c
Yohanes Eka Putra <ekaadev9@gmail.com>
2024-10-27 08:27:12 +0700

    Refactor: change minimum sdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "eka.giros"
-        minSdk = 25
+        minSdk = 26
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
